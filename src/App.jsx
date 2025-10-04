69203a67455fa97964cfdc8f6ef6d991308ad454
Alex Brilevski <brilevski.alex@gmail.com>
2025-10-04 15:12:10 +0300

    The NoProjectSelected component was added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
-import noProjectsImg from './assets/no-projects.png';
 import Sidebar from './components/Sidebar';
-import Button from './components/Button';
+import NoProjectSelected from './components/NoProjectSelected';
 import NewProject from './components/NewProject';
 import Project from './components/Project';
 
@@ -35,52 +34,40 @@ function App() {
   };
 
   const handleAddTask = (projectId, title) => {
-    setProjects(prevProjects => prevProjects.map(project => project.id === projectId ? {...project, tasks: [...project.tasks, {title}]} : project));
-    setSelectedProject(prevProject => ({...prevProject, tasks: [...prevProject.tasks, {title}]}));
+    setProjects(prevProjects => prevProjects.map(project => project.id === projectId ? { ...project, tasks: [...project.tasks, { title }] } : project));
+    setSelectedProject(prevProject => ({ ...prevProject, tasks: [...prevProject.tasks, { title }] }));
   };
 
   const handleDeleteTask = (projectId, taskIndex) => {
-    setProjects(prevProjects => prevProjects.map(project => project.id === projectId ? {...project, tasks: project.tasks.filter(task => project.tasks.indexOf(task) !== taskIndex)} : project));
-    setSelectedProject(prevProject => ({...prevProject, tasks: prevProject.tasks.filter(task => prevProject.tasks.indexOf(task) !== taskIndex)}));
+    setProjects(prevProjects => prevProjects.map(project => project.id === projectId ? { ...project, tasks: project.tasks.filter(task => project.tasks.indexOf(task) !== taskIndex) } : project));
+    setSelectedProject(prevProject => ({ ...prevProject, tasks: prevProject.tasks.filter(task => prevProject.tasks.indexOf(task) !== taskIndex) }));
   };
 
   return (
     <main className="h-screen my-8 flex gap-8">
-        <Sidebar
-          projects={projects}
-          handleProjectClick={handleSelectProject}
-          handleAddProjectClick={toggleNewProjectView}
+      <Sidebar
+        projects={projects}
+        handleProjectClick={handleSelectProject}
+        handleAddProjectClick={toggleNewProjectView}
+      />
+      {(!isNewProjectView && !selectedProject) &&
+        <NoProjectSelected toggleNewProjectView={toggleNewProjectView} />
+      }
+      {isNewProjectView &&
+        <NewProject
+          handleCancel={toggleNewProjectView}
+          handleSave={handleSaveProject}
         />
-        {(!isNewProjectView && !selectedProject) &&
-          <div className="flex flex-col items-center w-full">
-            <img className="w-16 h-16 object-contain mx-auto" src={noProjectsImg} alt="Project list" />
-            <h2 className="text-xl font-bold text-stone-700 my-4">
-              No project selected
-            </h2>
-            <p className="text-stone-600 mb-4">Select a project or get started with a new one</p>
-            <Button
-              className={'px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950'}
-              onClick={toggleNewProjectView}
-            >
-              Create new project
-            </Button>
-          </div>
-        }
-        {isNewProjectView &&
-          <NewProject
-            handleCancel={toggleNewProjectView}
-            handleSave={handleSaveProject}
-          />
-        }
-        {selectedProject &&
-          <Project
-            project={selectedProject}
-            handleDelete={handleDeleteProject}
-            handleAddTask={handleAddTask}
-            handleDeleteTask={handleDeleteTask}
-          />
-        }
-      </main>
+      }
+      {selectedProject &&
+        <Project
+          project={selectedProject}
+          handleDelete={handleDeleteProject}
+          handleAddTask={handleAddTask}
+          handleDeleteTask={handleDeleteTask}
+        />
+      }
+    </main>
   );
 }
 
---
+++ src/components/NoProjectSelected.jsx
@@ -0,0 +1,24 @@
+import noProjectsImg from '../assets/no-projects.png';
+import Button from './Button';
+
+const NoProjectSelected = ({ toggleNewProjectView }) => {
+  return (
+    <div className="mt-24 text-center w-2/3">
+      <img className="w-16 h-16 object-contain mx-auto" src={noProjectsImg} alt="An empty task list" />
+      <h2 className="text-xl font-bold text-stone-500 my-4">
+        No project selected
+      </h2>
+      <p className="text-stone-400 mb-4">Select a project or get started with a new one</p>
+      <p className="mt-8">
+        <Button
+          className={'px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950'}
+          onClick={toggleNewProjectView}
+        >
+          Create new project
+        </Button>
+      </p>
+    </div>
+  );
+};
+
+export default NoProjectSelected;
