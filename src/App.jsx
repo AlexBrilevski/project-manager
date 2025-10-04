305aaf6262d944ff9a939fc010d6c283e5e9af01
Alex Brilevski <brilevski.alex@gmail.com>
2025-10-04 13:55:51 +0300

    Main app container and sidebar component refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -45,8 +45,7 @@ function App() {
   };
 
   return (
-    <>
-      <main className="h-screen my-8 flex gap-8">
+    <main className="h-screen my-8 flex gap-8">
         <Sidebar
           projects={projects}
           handleProjectClick={handleSelectProject}
@@ -82,7 +81,6 @@ function App() {
           />
         }
       </main>
-    </>
   );
 }
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,24 +3,30 @@ import Button from "./Button";
 const Sidebar = ({ projects, handleProjectClick, handleAddProjectClick }) => {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
-      <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your projects</h2>
-      <Button
-        className={'px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100'}
-        onClick={handleAddProjectClick}
-      >
-        Add Project
-      </Button>
+      <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
+        Your projects
+      </h2>
+      <div>
+        <Button
+          className={'px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100'}
+          onClick={handleAddProjectClick}
+        >
+          + Add Project
+        </Button>
+      </div>
       {projects.length > 0 &&
-        <div className="flex flex-col gap-1 my-4">
+        <ul className="flex flex-col gap-1 my-4">
           {projects.map(project =>
-            <Button key={project.id}
-              className={'w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800'}
-              onClick={() => handleProjectClick(project.id)}
-            >
-              {project.title}
-            </Button>
+            <li key={project.id}>
+              <Button
+                className={'w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800'}
+                onClick={() => handleProjectClick(project.id)}
+              >
+                {project.title}
+              </Button>
+            </li>
           )}
-        </div>
+        </ul>
       }
     </aside>
   );
